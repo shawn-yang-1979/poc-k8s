96e5de01840ae12d7c38f7708b33b3d5725abddd
Shawn Yang <ocelot.yang.1979@gmail.com>
2025-04-23 01:03:51 +0800

    first mock test

--- openshift-client-example/openshift-client-example-main/src/test/java/com/example/MainTest.java
+++ openshift-client-example/openshift-client-example-main/src/test/java/com/example/MainTest.java
@@ -1,12 +1,27 @@
 package com.example;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import io.fabric8.kubernetes.api.model.PodBuilder;
+import io.fabric8.kubernetes.api.model.PodList;
+import io.fabric8.kubernetes.client.server.mock.EnableKubernetesMockClient;
+import io.fabric8.openshift.client.OpenShiftClient;
 
 @SpringBootTest
+@EnableKubernetesMockClient(crud=true)
 class MainTest {
 
-	@Test
-	void application_started() {
-	}
+  private static OpenShiftClient client;
+
+  @Test
+  void simpleTestWithTargetVersion() {
+    // CREATE
+    client.pods().inNamespace("ns1")
+        .resource(new PodBuilder().withNewMetadata().withName("pod1").endMetadata().build())
+        .create();
+    PodList podList = client.pods().inNamespace("ns1").list();
+    assertNotNull(podList);
+  }
+
 }
