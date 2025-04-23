554dea0fb9d6834fdb9120036f2ee400e91dd6b6
Shawn Yang <shawn.yang@ibm.com>
2025-04-23 21:34:04 +0800

    add more endpoints

--- openshift-client-example/openshift-client-example-adapter/src/main/java/com/example/adapter/rest/controller/OpenShiftRestController.java
+++ openshift-client-example/openshift-client-example-adapter/src/main/java/com/example/adapter/rest/controller/OpenShiftRestController.java
@@ -6,12 +6,11 @@
 package com.example.adapter.rest.controller;
 
 import java.util.List;
+import java.util.stream.Collectors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import io.fabric8.kubernetes.api.model.Pod;
 import io.fabric8.openshift.client.OpenShiftClient;
 
 @RestController
@@ -24,8 +23,27 @@ public class OpenShiftRestController {
     this.client = client;
   }
 
-  @GetMapping(path = "/{namespace}/pods")
-  public List<Pod> getPods(@PathVariable String namespace, @RequestParam String app) {
-    return client.pods().inNamespace(namespace).withLabel("app", app).list().getItems();
+  @GetMapping(path = "/projects")
+  public List<String> getProjects() {
+    return client.projects().list().getItems().stream().map(e -> e.getMetadata().getName())
+        .collect(Collectors.toList());
   }
+
+  @GetMapping(path = "/namespaces/{namespace}/pods")
+  public List<String> getPods(@PathVariable String namespace) {
+    return client.pods().inNamespace(namespace).list().getItems().stream()
+        .map(e -> e.getMetadata().getName()).collect(Collectors.toList());
+  }
+
+  @GetMapping(path = "/namespaces/{namespace}/pods/{pod}/pod-ip")
+  public String getPodIp(@PathVariable String namespace, @PathVariable String pod) {
+    return client.pods().inNamespace(namespace).withName(pod).get().getStatus().getPodIP();
+  }
+
+  @GetMapping(path = "/namespaces/{namespace}/deployments")
+  public List<String> getDeployments(@PathVariable String namespace) {
+    return client.apps().deployments().inNamespace(namespace).list().getItems().stream()
+        .map(e -> e.getMetadata().getName()).collect(Collectors.toList());
+  }
+
 }
