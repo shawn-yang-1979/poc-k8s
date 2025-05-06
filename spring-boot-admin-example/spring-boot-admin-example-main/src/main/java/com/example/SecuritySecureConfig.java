15b59bc46d9a1ce0894e106d47be121d4814c978
Shawn Yang <shawn.yang@ibm.com>
2025-05-06 19:30:07 +0800

    security

--- spring-boot-admin-example/spring-boot-admin-example-main/src/main/java/com/example/SecuritySecureConfig.java
+++ spring-boot-admin-example/spring-boot-admin-example-main/src/main/java/com/example/SecuritySecureConfig.java
@@ -2,13 +2,23 @@ package com.example;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import de.codecentric.boot.admin.server.config.AdminServerProperties;
 
+/**
+ * Reference:
+ * 
+ * https://docs.spring.io/spring-security/reference/5.7/servlet/configuration/java.html#_multiple_httpsecurity
+ * 
+ * https://docs.spring-boot-admin.com/2.7.14/#_securing_spring_boot_admin_server
+ * 
+ */
 @Configuration(proxyBeanMethods = false)
 public class SecuritySecureConfig {
   private final AdminServerProperties adminServer;
@@ -18,16 +28,26 @@ public class SecuritySecureConfig {
   }
 
   @Bean
-  SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+  @Order(1)
+  SecurityFilterChain actuatorFilterChain(HttpSecurity http) throws Exception {
+    http.antMatcher(this.adminServer.path("/actuator/**"))//
+        .authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated())
+        .sessionManagement(
+            session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .httpBasic(Customizer.withDefaults())//
+        .csrf(csrf -> csrf.disable());
+    return http.build();
+  }
+
+  @Bean
+  @Order(2)
+  SecurityFilterChain adminServerfilterChain(HttpSecurity http) throws Exception {
     SavedRequestAwareAuthenticationSuccessHandler successHandler =
         new SavedRequestAwareAuthenticationSuccessHandler();
     successHandler.setTargetUrlParameter("redirectTo");
     successHandler.setDefaultTargetUrl(this.adminServer.path("/"));
-
     http.authorizeHttpRequests(authorizeRequests -> authorizeRequests //
         .antMatchers(this.adminServer.path("/assets/**")).permitAll() // <1>
-        .antMatchers(this.adminServer.path("/actuator/info")).permitAll()//
-        .antMatchers(this.adminServer.path("/actuator/health")).permitAll()//
         .antMatchers(this.adminServer.path("/login")).permitAll()//
         .anyRequest().authenticated()) // <2>
         .formLogin(formLogin -> formLogin//
@@ -37,10 +57,7 @@ public class SecuritySecureConfig {
         .logout(logout -> logout//
             .logoutUrl(this.adminServer.path("/logout")))//
         .csrf(csrf -> csrf//
-            .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()) // <5>
-            .ignoringRequestMatchers(
-                new AntPathRequestMatcher(this.adminServer.path("/actuator/**")) // <7>
-            ));
+            .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()));
     return http.build();
   }
 }
